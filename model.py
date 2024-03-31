d5cfea29b22ccd1355a2f49c4a68856779b72212
kritik1104 <mohantykritik62@gmail.com>
2024-03-31 12:14:27 +0530

    Added new ML Model code

--- model.py
+++ model.py
@@ -1,1 +1,11 @@
 print("Adding all ML related code here ")
+
+def model1():
+    print("Adding code for ML Model 1")
+
+    def model2():
+    print("Adding code for ML Model 2")
+
+
+    def model3():
+    print("Adding code for ML Model 3")
